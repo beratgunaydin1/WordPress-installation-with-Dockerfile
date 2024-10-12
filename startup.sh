23e6a07a8a9864306399a229cddb2f162c917f29
beratgunaydin1 <berat_5353@msn.com>
2024-10-12 15:13:15 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-
 # Nginx ve PHP-FPM'i başlatıyoruz
 service php7.4-fpm start
 service nginx start
-
 # Konteynerin açık kalması için sonsuz bir döngü başlatıyoruz
 tail -f /var/log/nginx/access.log
 
